d9e5b93ca716a2149634e6a2448cdf7621f13345
lamekammi <kameronnb@gmail.com>
2022-06-28 10:28:09 -0700

    adding passport.js

--- models/user.js
+++ models/user.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: String
+    name: String,
+    googleId: {
+        type: String,
+        requiered: true
+    },
+    email: String,
+    avatar: String
 }, {
     timestamps: true
 });
