4d423df71efd2840b677e866e228b1f418782893
lamekammi <kameronnb@gmail.com>
2022-06-28 11:26:45 -0700

    adding manga.js model

---
+++ models/manga.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const mangaSchema = new Schema({
+    title: String,
+    chapter: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    reading: {
+        type: String,
+        enum: ['Currently Reading', 'Plan To Read', 'On Hold', 'Completed', 'Dropped'],
+        default: 'Currently Reading'
+    },
+    rating: {
+        type: Number,
+        enum: [1, 2, 3, 4, 5],
+        default: 5
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Manga', mangaSchema);
